96132310f1a49dd65a5e83f9efdc50fccab29bce
sungwoo.kim <tjddn5242@gmail.com>
2023-01-17 20:33:27 +0900

    stt 연ㄱㄹ

--- main.py
+++ main.py
@@ -3,50 +3,45 @@ from audio_recorder_streamlit import audio_recorder
 from io import BytesIO
 from pydub import AudioSegment
 import speech_recognition as sr
+import STT
+import time
 
-# import numpy as np
-# from scipy.io.wavfile import write
-# import soundfile as sf
-# import wave
-# import streamlit.components.v1 as components
-# import os
-
-# sound_chunk = pydub.AudioSegment.empty()
-
-# for audio_frame in audio_bytes:
-#     sound = pydub.AudioSegment(
-#                         data=audio_frame.to_ndarray().tobytes(),
-#                         sample_width=audio_frame.format.bytes,
-#                         frame_rate=audio_frame.sample_rate,
-#                         channels=len(audio_frame.layout.channels),
-#                     )
-#     sound_chunk += sound
-
-# sound.export('./testttt.wav', format='wav')
 st.title("Audio Recorder")
+
 
 
 audio_bytes = audio_recorder("Click to record", pause_threshold=10.0)
 if audio_bytes:
+    
     st.audio(audio_bytes, format="audio/wav")
+    with open("audio.wav", "wb") as f:
+        f.write(audio_bytes)
+    # wav_file = open("audio.wav", "wb")
+    # wav_file.write(audio_bytes)
+    # wav_file.close()
 
-AudioSegment.from_raw(BytesIO(audio_bytes), sample_width=2, frame_rate=32000, channels=2).export('./testttt.wav', format='wav')
+
+# AudioSegment.from_raw(BytesIO(audio_bytes), sample_width=2, frame_rate=32000, channels=2).export('./testttt.wav', format='wav')
 # wav_file = open("audio.wav", "wb")
 # wav_file.write(audio_bytes.tobytes()).export('./asdf.wav', format = 'wav')
 
-r = sr.Recognizer()
+id = STT.BitoPost("audio.wav")
+time.sleep(5)
+result = STT.BitoGet(id)
+st.markdown(f'{result}')
 
-test = sr.AudioFile('./testttt.wav')
-with test as source:
-    audio = r.record(source)
+# r = sr.Recognizer()
+
+# test = sr.AudioFile('audio.wav')
+# with test as source:
 
 
-result = r.recognize_google(audio, language = 'ko-KR', show_all=True)['alternative']
-rr = []
-for i in result:
-    rr.append(i['transcript'])
-rr = '.'.join(rr)
-st.markdown(f'{rr}')
+# result = r.recognize_google(audio, language = 'ko-KR', show_all=True)['alternative']
+# rr = []
+# for i in result:
+#     rr.append(i['transcript'])
+# rr = '.'.join(rr)
+# st.markdown(f'{rr}')
 # print(result['alternative'][0]['transcript'])
 # print(result)
 
