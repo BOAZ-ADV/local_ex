2331e46405a21c4965b2965a069b7588a1ec3188
jurio3o <102223289+jurio3o@users.noreply.github.com>
2023-01-22 14:29:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,34 +20,6 @@ def speech_to_text(DIR: str):
     result = STT.BitoGet(id)
     return result
 
-# def show_waveframe(wav):
-
-
-
-# def trim_audio_data(start, end, audio_file, save_file):
-#     sr = 96000
-
-#     y, sr = librosa.load(audio_file, sr=sr)
-
-#     ny = y[sr*start :sr*end]
-
-#     librosa.output.write_wav(save_file, ny, sr)
-
-# def chop_audio(file_path, segment_size):
-#     audio_data = AudioSegment.from_wav(file_path)
-#     chopped_audio = [x for x in audio_data[::segment_size]]
-#     return chopped_audio
-
-# def trim_audio(audio_data, second):
-#     second = 1
-#     seconds = second * 1000
-#     slice = []
-#     for i in range(int(math.ceil(len(audio_data)/seconds))):
-#         slice.append(audio_data[i*seconds:seconds*(i+1)])
-#     #     slice.export('newSong_{}.mp3'.format(i), format="mp3")
-#     return slice
-
-
 
 def main():
     st.title('👮보이스피싱 잡아라👮')
@@ -62,21 +34,18 @@ def main():
     if audio_bytes:
         with open("audio.wav", "wb") as f:
             f.write(audio_bytes)
-        # st.text('recording')
-        # text_result = speech_to_text("audio.wav")
-        # text_data.append(text_result)
-        # text_data = []
+
         result_dict = {}
         text_data = speech_to_text("audio.wav")
-        st.text('stt 진행 중 ')
-        # st.text(text_data)
+        st.text('stt 진행 중')
+
 
         st.text('call classification model & encoder')
         model = joblib.load('best_f1_model.pkl')
         encoder = joblib.load('best_tfvec.pkl')
         
 
-        slice_num = 5
+        slice_num = 5 #slice 할 글자 수
         for i in range(round(len(text_data)/slice_num)):
             text = text_data[ : slice_num*(1+i)]
             array = model.predict_proba(encoder.transform([text]))
@@ -88,11 +57,12 @@ def main():
         df = pd.DataFrame.from_dict([result_dict]).transpose().reset_index()
         df.columns = ['text_length', 'prob']
         st.dataframe(df)
-        fig = px.bar(df, x='text_length', y='prob')
+        fig = px.area(df, x='text_length', y='prob', markers = True)
         
         tab1, tab2 = st.tabs(["output text", "plot"])
         with tab1:
             st.markdown(f'결과: {text_data}')
+            st.text(1-prob)
 
         with tab2:
             st.plotly_chart(fig, theme=None)
